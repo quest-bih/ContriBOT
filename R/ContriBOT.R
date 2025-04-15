480a9672a5240e8df7d8fd533935582164092112
Nachev <vladislav.nachev@charite.de>
2025-04-15 10:24:16 +0200

    refactoring functions in separate files

--- R/ContriBOT.R
+++ R/ContriBOT.R
@@ -18,68 +18,44 @@
 #' @export
 contribution_detection <- function(PDF_text_sentences)
 {
+  keyword_list <- .create_keyword_list()
 
-  # credit_regex <- c("manuscript\\.?$",
-  #                   "editing",
-  #                   "version",
-  #                   "paper",
-  #                   "work",
-  #                   "project\\.$",
-  #                   "entirety\\.$",
-  #                   "validation",
-  #                   "review",
-  #                   "analysis",
-  #                   "investigation",
-  #                   "methodo",
-  #                   "supervision",
-  #                   "\\(equal\\)",
-  #                   "writing",
-  #                   "contributors?",
-  #                   "resources",
-  #                   "author",
-  #                   "conceptuali(z|s)ation",
-  #                   "draft"
+  # TODO: refactor regexes to use yaml function
+  # credit_section_list <- c(
+  #   "Author\\W?s?\\W? ?Contributions? Statement",
+  #   "A ?u ?t ?h ?o ?r ?(\\W?s?|s? ?\\W?)? ?C ?o ?n ?t ?r ?i ?b ?u ?t ?i ?o ?n ?s?",
+  #   "CRediT Authors?hip? Contributions?( Statement)?",
+  #   "CRediT Author Statement",
+  #   "Author\\W?s?\\W?s responsibilities",
+  #   "Contribut.* Statement",
+  #   # "Contributor’s Statement"
+  #   "Contributions$|Contributions:",
+  #   "C ?O ?N ?T ?R ?I ?B ?U ?T ?I ?O ?N ?S ?O ?F ?A ?U ?T ?H ?O ?R ?S",
+  #   "AUTHOR CONTRIBUTORS",
+  #   "Author statements?",
+  #   "C ?o ?n ?t ?r ?i ?b ?u ?t ?o ?r ?s", # TODO: check this for 10.1136 $ or no $
+  #   # paste0("(", "Contributors .* (", credit_regex, "))"),
+  #   "Authorship",
+  #   "Author roles",
+  #   "Name Location Contribution",
+  #   "Description of authors\\W? roles"
   # ) |>
-  #   paste(collapse = "|")
-
-
-  credit_section_list <- c(
-    "Author\\W?s?\\W? ?Contributions? Statement",
-    "A ?u ?t ?h ?o ?r ?(\\W?s?|s? ?\\W?)? ?C ?o ?n ?t ?r ?i ?b ?u ?t ?i ?o ?n ?s?",
-    "CRediT Authors?hip? Contributions?( Statement)?",
-    "CRediT Author Statement",
-    "Author\\W?s?\\W?s responsibilities",
-    "Contribut.* Statement",
-    # "Contributor’s Statement"
-    "Contributions$|Contributions:",
-    "C ?O ?N ?T ?R ?I ?B ?U ?T ?I ?O ?N ?S ?O ?F ?A ?U ?T ?H ?O ?R ?S",
-    "AUTHOR CONTRIBUTORS",
-    "Author statements?",
-    "C ?o ?n ?t ?r ?i ?b ?u ?t ?o ?r ?s", # TODO: check this for 10.1136 $ or no $
-    # paste0("(", "Contributors .* (", credit_regex, "))"),
-    "Authorship",
-    "Author roles",
-    "Name Location Contribution",
-    "Description of authors\\W? roles"
-  ) |>
-    .format_keyword_vector() |>
-    stringr::str_replace_all("w", "W")
+  #   .format_keyword_vector() |>
+  #   stringr::str_replace_all("w", "W")
 
 
 # str_detect("credit authors contributions", "credit authors?(hip)? contributions?(statement)?")
-  ackn_section_list <- c(
-    "A ?c ?k ?n ?o ?w ?l ?e ?d ?g ?e? ?m ?e ?n ?t ?s?",
-    "We (would like to )?thank",
-    "Additional contributions"
-  ) |>
-    .format_keyword_vector()
-
-
-
-  orcid_section_list <- c(
-    "ORCID"
-  ) |>
-    .format_keyword_vector()
+#   ackn_section_list <- c(
+#     "A ?c ?k ?n ?o ?w ?l ?e ?d ?g ?e? ?m ?e ?n ?t ?s?",
+#     "We (would like to )?thank",
+#     "Additional contributions"
+#   ) |>
+#     .format_keyword_vector()
+# keyword_list$ackn_section
+#   orcid_section_list <- c(
+#     "ORCID"
+#   ) |>
+#     .format_keyword_vector()
 
   # PDF_text_sentences <- pdf_text_corpus
 
@@ -87,15 +63,22 @@ contribution_detection <- function(PDF_text_sentences)
   # tib <- tibble(text = PDF_text_sentences[[1]])
   print("Extracting Contributions...")
   contrib_text_sentences <- PDF_text_sentences |>
-    furrr::future_map(\(x) .extract_section(x, credit_section_list, look_in_tables = TRUE), .progress = TRUE)
+    furrr::future_map(\(x) .extract_section(x,
+                                            keyword_list$autorship_section,
+                                            look_in_tables = TRUE),
+                      .progress = TRUE)
 
   print("Extracting Acknowledgements...")
   ackn_text_sentences <- PDF_text_sentences |>
-    furrr::future_map(\(x) .extract_section(x, ackn_section_list), .progress = TRUE)
+    furrr::future_map(\(x) .extract_section(x,
+                                            keyword_list$ackn_section),
+                      .progress = TRUE)
 
   print("Extracting ORCIDs...")
   orcid_text_sentences <- PDF_text_sentences |>
-    furrr::future_map(\(x) .extract_section(x, orcid_section_list), .progress = TRUE)
+    furrr::future_map(\(x) .extract_section(x,
+                                            keyword_list$orcid_section),
+                      .progress = TRUE)
 
   ackn_results <- ackn_text_sentences |>
     .enframe_results(name = "article", value = "ackn_statement")
@@ -129,11 +112,12 @@ contribution_detection <- function(PDF_text_sentences)
 .extract_section <- function(PDF_text_sentences, section_regexes, look_in_tables = FALSE) {
 
   # TODO: validate that text extraction stops at the right spots for ackn and credit
+  keyword_list <- .create_keyword_list()
 
-  table_titles <- c("appendix( \\d)? authors")
-
-  section_string <- paste0("(<section>)[^\\w+][\\d,^\\w]*(", section_regexes, ")\\b")
-  if (look_in_tables == TRUE) section_string <- paste0(section_string, "|", table_titles)
+  section_string <- paste0("(<section>|#+)[^\\w+][\\d,^\\w]*(", section_regexes, ")\\b")
+  if (look_in_tables == TRUE) {
+    section_string <- paste0(section_string, "|", keyword_list$table_credit) 
+  }
   # stringr::str_detect(PDF_text_sentence, data_availability)
   section_detections <- furrr::future_map_lgl(PDF_text_sentences,
                                           \(sentence) stringr::str_detect(sentence, stringr::regex(section_string, ignore_case = TRUE)))
@@ -163,38 +147,40 @@ contribution_detection <- function(PDF_text_sentences)
 
   # section_regexes
 
-  stop_regex <- c(
-    "c ?o ?n ?f ?l ?i ?c ?t ?s?",
-    "o ?f ?i ?n ?t ?e ?r ?e ?s ?t",
-    "r ?e ?f ?e ?r ?e ?n ?c ?e ?s",
-    "disclaimer",
-    "availability",
-    "competing",
-    "prior presentation",
-    "specialty section",
-    "d ?e ?c ?l ?a ?r ?a ?t ?i ?o ?n",
-    "additional information:",
-    "a ?c ?k ?n ?o ?w ?l ?e ?d ?g ?e? ?m ?e ?n ?t ?s?",
-    "orcid",
-    "<section>(\\) sources of)? funding",
-    "disclosures?",
-    "peer",
-    "ethic",
-    "keywords",
-    "data .* statement",
-    "credit author statement",
-    "author contributions",
-    "accepted \\d{1,2}",
-    "\\u00a9",
-    "citation:",
-    "<section> open data",
-    "supplement",
-    "et al\\.",
-    "abbreviations",
-    "twitter",
-    "receive"
-    )
+  # stop_regex <- c(
+  #   "c ?o ?n ?f ?l ?i ?c ?t ?s?",
+  #   "o ?f ?i ?n ?t ?e ?r ?e ?s ?t",
+  #   "r ?e ?f ?e ?r ?e ?n ?c ?e ?s",
+  #   "disclaimer",
+  #   "availability",
+  #   "competing",
+  #   "prior presentation",
+  #   "specialty section",
+  #   "d ?e ?c ?l ?a ?r ?a ?t ?i ?o ?n",
+  #   "additional information:",
+  #   "a ?c ?k ?n ?o ?w ?l ?e ?d ?g ?e? ?m ?e ?n ?t ?s?",
+  #   "orcid",
+  #   "<section>(\\) sources of)? funding",
+  #   "disclosures?",
+  #   "peer",
+  #   "ethic",
+  #   "keywords",
+  #   "data .* statement",
+  #   "credit author statement",
+  #   "author contributions",
+  #   "accepted \\d{1,2}",
+  #   "\\u00a9",
+  #   "citation:",
+  #   "<section> open data",
+  #   "supplement",
+  #   "et al\\.",
+  #   "abbreviations",
+  #   "twitter",
+  #   "receive"
+  #   )
 
+  stop_regex <- keyword_list$stop_sections
+  
   stop_regex <- stop_regex[!stringr::str_detect(stop_regex, section_regexes)] |>
     paste(collapse = "|")
 
@@ -210,46 +196,12 @@ contribution_detection <- function(PDF_text_sentences)
 
   } else {
 
-    # if (stringr::str_detect(section_regexes, "contribution")) {
-
       section_end_candidates <- furrr::future_map_lgl(PDF_text_sentences[(section_start + 1):length(PDF_text_sentences)],
                                            \(sentence) stringr::str_detect(sentence, stringr::regex(stop_regex, ignore_case = TRUE))) |>
         which() - 1
 
       section_end <- section_end_candidates[1]
 
-    # } else {
-    #
-    #   section_end_candidates <- furrr::future_map_lgl(PDF_text_sentences[(section_start + 1):length(PDF_text_sentences)],
-    #                                                   # \(sentence) stringr::str_detect(sentence, "section> (?!d )|^\\d\\.|section> references")) |>
-    #                                                   \(sentence) stringr::str_detect(sentence, stop_regex)) |>
-    #     which() - 1
-    #   # check if candidates are full sentences ending in full stop. This achieves splicing if section contines on next page
-    #   completed_sentences <- furrr::future_map_lgl(PDF_text_sentences[section_start + section_end_candidates],
-    #                                                \(sentence) stringr::str_detect(sentence, "\\.$"))
-    #
-    #   if (stringr::str_length(str_section_sameline) < 5 & str_section_sameline != ".") {
-    #     # first_sentence <- DAS_start + 1
-    #
-    #     section_end <- section_end_candidates[-1][completed_sentences[-1]][1]#
-    #
-    #   } else {
-    #     section_end <- section_end_candidates[completed_sentences][1] # the first complete sentence before the beginning of a section
-    #
-    #   }
-    #
-    # }
-
-    # if (section_start / length(section_detections) < 0.1 & section_end != 0) { # for plos journals with DAS on first page
-    #
-    #   section_second_part <- furrr::future_map_lgl(PDF_text_sentences[(section_start + section_end + 1):length(PDF_text_sentences)],
-    #                                            \(sentence) stringr::str_detect(sentence, "<section> funding:"))
-    #   if (sum(section_second_part) == 0) {
-    #     section_end <- section_end
-    #   } else {
-    #     section_end <- section_end + which(section_second_part) - 1
-    #   }
-    # }
   }
 
   if (is.na(section_end)) section_end <- 0
@@ -268,85 +220,6 @@ contribution_detection <- function(PDF_text_sentences)
 }
 
 
-#' standardize the format of different keyword vectors
-#' @noRd
-.format_keyword_vector <- function(keywords, end_boundary = FALSE) {
-  #typically word boundaries are added in the beginning only to allow for different possible endings
-  if(end_boundary) {
-    keywords_formatted <- paste0("\\b", keywords, "\\b")
-  } else {
-    keywords_formatted <- paste0("\\b", keywords)
-  }
-  #collapse keywords into one string with OR symbol between them and convert to lowercase
-  keywords_formatted <- paste(keywords_formatted, collapse = "|") |> tolower()
 
-  return(keywords_formatted)
-}
 
-# pdf_folder <- "C:/Users/Vladi/OneDrive - Charité - Universitätsmedizin Berlin/PDFs_22/"
-#
-# PDF_file <- paste0(pdf_folder, "dir2/10.1002+alz.12365.pdf")
-
-#' Seach for ORCID hyperlinks and extract ORCIDs.
-#'
-#' The algorithm searchers for an ORCID and extracts what it finds.
-#'
-#' @param PDF_file String with the path to the PDF to be screened.
-#'
-#' @return String with the ORCIDs
-#'
-#' @examples
-#' \dontrun{
-#' extract_orcid_hyperlink(some_pdf)
-#' }
-#'
-#' @export
-
-extract_orcid_hyperlink <- function(PDF_file) {
-  orcids <- readr::read_file_raw(PDF_file) |>
-    furrr::future_map_chr(rawToChar) |>
-    paste(collapse = "") |>
-    stringr::str_extract_all(stringr::regex("(https?\\://orcid\\.org/\\d{4}-\\d{4}-\\d{4}-\\w{4})|(https?.*orcid.*(\\d|X))",
-                             ignore_case = TRUE)) |>
-    unlist() |>
-    unique() |>
-    stringi::stri_unescape_unicode()
-
-   # stringr::str_view("https://orcid.org/000-0002-1162-1318)>>/Type/Annot/Subtype/Link/Rect[301.436 642.104 309.316 652.422]/Border[0 0 0; https://orcid.org/0000-0001-5332-6811; https://orcid.org/0000-0002-4232-3305", "(https?.*orcid.*(\\d|X)(?=(\\)|,)))")
-
-# stringr::str_view("http\072\057\057orcid\056org\0570000\0550001\0556389\0550029", "(https?\\://orcid\\.org/\\d{4}-\\d{4}-\\d{4}-\\w{4})|
-#   (https?\072\057\057orcid\056org\057\\d{4}\055\\d{4}\055\\d{4}\055\\w{4})")
-  if (length(orcids) > 1) {
-    return(paste(orcids, collapse = "; "))
-  } else if (length(orcids) == 0) {
-    return("")
-
-  } else {
-      return(orcids)
-    }
-}
-
-# tib <- tibble(orcids)
-#' Seach for ORCID hyperlinks and extract ORCIDs from a folder of PDF files.
-#'
-#' The algorithm searchers for an ORCID and extracts what it finds from each file.
-#'
-#' @param pdf_folder String with the path to folder with PDF files to be screened.
-#'
-#' @return Tibble with one row per screened file and the file name and orcids extracted,
-#' as well as a logical value for orcids detected as columns.
-#'
-#' @examples
-#' \dontrun{
-#' extract_orcids_from_folder(pdf_folder)
-#' }
-#'
-#' @export
-extract_orcids_from_folder <- function(pdf_folder) {
-
-  PDF_files <- list.files(pdf_folder, full.names = TRUE)
-
-  furrr::future_map_chr(PDF_files, extract_orcid_hyperlink,
-                        .progress =  TRUE)
-}
 
