2ab74b387731a583ecea07bba164808de523d0d8
ontogenerator <vladislav.nachev@charite.de>
2025-07-01 17:02:11 +0200

    internal variable naming cleanup

--- R/ContriBOT.R
+++ R/ContriBOT.R
@@ -3,7 +3,7 @@
 #' The algorithm searchers for a  Authorship, Acknowledgement, and ORCID statements
 #' and extracts them.
 #'
-#' @param pdf_text_sentences Document corpus loaded with the `oddpub::pdf_load` function.
+#' @param text_sentences Document corpus loaded with the `oddpub::pdf_load` function.
 #' @importFrom rlang .data
 #'
 #' @return Tibble with one row per screened document and the file name and logical values for authorship,
@@ -16,24 +16,24 @@
 #' }
 #'
 #' @export
-extract_contributions <- function(pdf_text_sentences)
+extract_contributions <- function(text_sentences)
 {
   keyword_list <- .create_keyword_list()
 
   message("Extracting Contributions...")
-  contrib_text_sentences <- pdf_text_sentences |>
+  contrib_text_sentences <- text_sentences |>
     .extract_section_progress(
       keyword_list$autorship_section,
       look_in_tables = TRUE)
 
   message("Extracting Acknowledgements...")
-  ackn_text_sentences <- pdf_text_sentences |>
+  ackn_text_sentences <- text_sentences |>
     .extract_section_progress(
       keyword_list$ackn_section,
       look_in_tables = FALSE)
 
   message("Extracting ORCIDs...")
-  orcid_text_sentences <- pdf_text_sentences |>
+  orcid_text_sentences <- text_sentences |>
     .extract_section_progress(
       keyword_list$orcid_section,
       look_in_tables = FALSE)
@@ -61,9 +61,26 @@ extract_contributions <- function(pdf_text_sentences)
     tibble::enframe(name = name, value = value)
 }
 
+#' remove inserts
+#' @noRd
+.remove_inserts <- function(text_sentences) {
+  tibble::tibble(text = text_sentences, mask = 0) |>
+    dplyr::mutate(mask = ifelse(stringr::str_detect(text, "<insert>"), 1, mask)) |>
+    dplyr::mutate(mask = cumsum(mask)) |>
+
+    dplyr::group_by(mask) |>
+    dplyr::mutate(is_gap = stringr::str_detect(dplyr::lag(text, default = ""), "<iend>") &
+                    !stringr::str_detect(text, "<insert>"),
+                  has_insert = any(stringr::str_detect(text, "<insert>")),
+                  gapsum = cumsum(is_gap)) |>
+    dplyr::mutate(gapsum = ifelse(has_insert == FALSE, 1, gapsum)) |>
+    dplyr::filter(gapsum != 0) |>
+    dplyr::pull(text)
+}
+
 #' extract section
 #' @noRd
-.extract_section <- function(pdf_text_sentences, section_regexes, look_in_tables = FALSE) {
+.extract_section <- function(text_sentences, section_regexes, look_in_tables = FALSE) {
 
   # TODO: validate that text extraction stops at the right spots, also for ORCID
   keyword_list <- .create_keyword_list()
@@ -71,10 +88,12 @@ extract_contributions <- function(pdf_text_sentences)
   section_string <- paste0("(<section>|#+)[^\\w+][\\d,^\\w]*(", section_regexes, ")\\b")
   if (look_in_tables == TRUE) {
     section_string <- paste0(section_string, "|", keyword_list$table_credit)
+  } else {
+    text_sentences <- .remove_inserts(text_sentences)
   }
 
   section_detections <-
-    furrr::future_map_lgl(pdf_text_sentences,
+    furrr::future_map_lgl(text_sentences,
                           \(sentence) stringr::str_detect(
                             sentence,
                             stringr::regex(section_string, ignore_case = TRUE)))
@@ -92,7 +111,7 @@ extract_contributions <- function(pdf_text_sentences)
     return("")
   }
 
-  str_section <- pdf_text_sentences[section_start] |>
+  str_section <- text_sentences[section_start] |>
     stringr::str_trim()
   str_section_sameline <- str_section |>
     stringr::str_remove(stringr::regex(section_string, ignore_case = TRUE))
@@ -105,13 +124,13 @@ extract_contributions <- function(pdf_text_sentences)
 
   # candidates are sentences after the first section but before the next
   # which begin with <section> or digit. (reference number at start of line)
-  is_plos <- any(stringr::str_detect(pdf_text_sentences[1:10], "^plos ")) #explosive?
+  is_plos <- any(stringr::str_detect(text_sentences[1:10], "^plos "), na.rm = TRUE) #explosive?
 
   if (is_plos == TRUE) {
 
     section_end <-
       furrr::future_map_lgl(
-        pdf_text_sentences[(section_start + 1):length(pdf_text_sentences)],
+        text_sentences[(section_start + 1):length(text_sentences)],
         \(sentence) stringr::str_detect(sentence,
                                         stringr::regex("section> references",
                                                        ignore_case = TRUE))) |>
@@ -121,7 +140,7 @@ extract_contributions <- function(pdf_text_sentences)
 
       section_end_candidates <-
         furrr::future_map_lgl(
-          pdf_text_sentences[(section_start + 1):length(pdf_text_sentences)],
+          text_sentences[(section_start + 1):length(text_sentences)],
           \(sentence) stringr::str_detect(sentence,
                                           stringr::regex(stop_regex,
                                                          ignore_case = TRUE))) |>
@@ -134,11 +153,12 @@ extract_contributions <- function(pdf_text_sentences)
   if (is.na(section_end)) section_end <- 0
   section_end <- section_start + section_end
 
-  section <- pdf_text_sentences[section_start:section_end]
+  section <- text_sentences[section_start:section_end]
 
-  if (section_start < 50 & any(stringr::str_detect(pdf_text_sentences[1:10],
+  if (section_start < 50 & any(stringr::str_detect(text_sentences[1:10],
                                                    stringr::regex("plos",
-                                                                  ignore_case = TRUE)))) {
+                                                                  ignore_case = TRUE)),
+                               na.rm = TRUE)) {
     section <- oddpub::splice_plos_twopager(section)
   }
   section |>
@@ -148,11 +168,11 @@ extract_contributions <- function(pdf_text_sentences)
 
 #' extract contribution section with progress bar
 #' @noRd
-.extract_section_progress <- function(pdf_text_sentences,
+.extract_section_progress <- function(text_sentences,
                                       keywords,
                                       look_in_tables) {
-  p <- progressr::progressor(along = pdf_text_sentences)
-  pdf_text_sentences |>
+  p <- progressr::progressor(along = text_sentences)
+  text_sentences |>
     furrr::future_map(\(x) {
       p()
       .extract_section(x,
